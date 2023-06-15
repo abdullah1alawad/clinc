d8c8b9c3cc18c6174fcfa1fdc9611958edcfd864
abdullah1alawad <abdullahalawad23@gmail.com>
2023-06-15 22:26:00 +0300

    ya basseeim

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,8 +4,52 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use const http\Client\Curl\PROXY_HTTP;
 
 class Student extends Model
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'fu_id','university_id',
+        'level','semester',
+        'email','email_verified_at',
+        'created_at','updated_at',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'email_verified_at',
+        'created_at','updated_at',
+    ];
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [];
+
+    public function student(){
+        return $this->belongsTo(User::class);
+    }
+    public function student_marks(){
+        return $this->hasMany(Student_mark::class);
+    }
+    public function processes(){
+        return $this->hasMany(Process::class);
+    }
+    public function patents()
+    {
+        return $this->hasMany(Patient::class);
+    }
 }
