7820ac5fbe41a104631503896ce545bdd98aa503
abdullah1alawad <abdullahalawad23@gmail.com>
2023-07-23 02:53:46 +0300

    hi

--- resources/views/student/editProfile.blade.php
+++ resources/views/student/editProfile.blade.php
@@ -8,40 +8,38 @@
 @section('content')
 
     <div class="container emp-profile">
-<<<<<<< HEAD
-        <form action="{{route('doctor.profile.update')}}" method="post" enctype="multipart/form-data">
-            @method('put')
-            @csrf
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="profile-img">
-                        <img
-                            src="{{asset('images/'.$user->photo)}}"
-                            alt=""/>
-                        <div class="file btn btn-lg btn-primary">
-                            Change Photo
-                            <input type="file" name="photo"/>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="profile-head">
-                        <h5>
-                            {{$user->name}}
-                        </h5>
+{{--        <form action="{{route('doctor.profile.update')}}" method="post" enctype="multipart/form-data">--}}
+{{--            @method('put')--}}
+{{--            @csrf--}}
+{{--            <div class="row">--}}
+{{--                <div class="col-md-4">--}}
+{{--                    <div class="profile-img">--}}
+{{--                        <img--}}
+{{--                            src="{{asset('images/'.$user->photo)}}"--}}
+{{--                            alt=""/>--}}
+{{--                        <div class="file btn btn-lg btn-primary">--}}
+{{--                            Change Photo--}}
+{{--                            <input type="file" name="photo"/>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <div class="col-md-6">--}}
+{{--                    <div class="profile-head">--}}
+{{--                        <h5>--}}
+{{--                            {{$user->name}}--}}
+{{--                        </h5>--}}
 
-                        <ul class="nav nav-tabs" id="myTab" role="tablist">
-                            <li class="nav-item">
-                                <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab"
-                                   aria-controls="home" aria-selected="true">About</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab"
-                                   aria-controls="profile" aria-selected="false">Change Password</a>
-                            </li>
-                        </ul>
-                    </div>
-=======
+{{--                        <ul class="nav nav-tabs" id="myTab" role="tablist">--}}
+{{--                            <li class="nav-item">--}}
+{{--                                <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab"--}}
+{{--                                   aria-controls="home" aria-selected="true">About</a>--}}
+{{--                            </li>--}}
+{{--                            <li class="nav-item">--}}
+{{--                                <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab"--}}
+{{--                                   aria-controls="profile" aria-selected="false">Change Password</a>--}}
+{{--                            </li>--}}
+{{--                        </ul>--}}
+{{--                    </div>--}}
         <div class="row">
             <div class="col-md-4">
                 <form action="{{route('student.change.photo')}}" method="post" enctype="multipart/form-data">
@@ -68,7 +66,7 @@
                     <h6>
                         The Best Dentist In The World
                     </h6>
->>>>>>> c7f1aac7d67c4063e2892cff70d54ec2e5760920
+
                 </div>
             </div>
         </div>
@@ -98,12 +96,11 @@
                                 </div>
                                 <div class="col-md-6">
                                     <input type="text" name="name" value="{{$user->name}}">
-<<<<<<< HEAD
-=======
+
                                     @error('name')
                                     <div class="text-danger">{{ $message }}</div>
                                     @enderror
->>>>>>> c7f1aac7d67c4063e2892cff70d54ec2e5760920
+
                                 </div>
                             </div>
                             <div class="row py-1">
@@ -112,12 +109,9 @@
                                 </div>
                                 <div class="col-md-6">
                                     <input type="email" name="email" value="{{$user->email}}">
-<<<<<<< HEAD
-=======
                                     @error('email')
                                     <div class="text-danger">{{ $message }}</div>
                                     @enderror
->>>>>>> c7f1aac7d67c4063e2892cff70d54ec2e5760920
                                 </div>
                             </div>
                             <div class="row py-1">
@@ -126,12 +120,11 @@
                                 </div>
                                 <div class="col-md-6">
                                     <input type="text" name="phone" value="{{$user->phone}}">
-<<<<<<< HEAD
-=======
+
                                     @error('phone')
                                     <div class="text-danger">{{ $message }}</div>
                                     @enderror
->>>>>>> c7f1aac7d67c4063e2892cff70d54ec2e5760920
+
                                 </div>
                             </div>
                             <div class="row py-1">
@@ -140,12 +133,11 @@
                                 </div>
                                 <div class="col-md-6">
                                     <input type="text" name="national_id" value="{{$user->national_id}}">
-<<<<<<< HEAD
-=======
+
                                     @error('national_id')
                                     <div class="text-danger">{{ $message }}</div>
                                     @enderror
->>>>>>> c7f1aac7d67c4063e2892cff70d54ec2e5760920
+
                                 </div>
                             </div>
                             <div class="row py-2">
@@ -154,12 +146,9 @@
                                 </div>
                                 <div class="col-md-6">
                                     <div class="radio-container">
-<<<<<<< HEAD
                                         <input type="radio" id="male" name="gender" value="0" checked>
-=======
                                         <input type="radio" id="male" name="gender"
                                                value="0" {{($user->gender == 'Male') ? 'checked' : ''}}>
->>>>>>> c7f1aac7d67c4063e2892cff70d54ec2e5760920
                                         <label for="male">Male</label>
                                     </div>
 
@@ -224,27 +213,25 @@
                     </div>
                 </div>
             </div>
-<<<<<<< HEAD
-            @if($errors->any())
-                <div class="error-message">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
+{{--            @if($errors->any())--}}
+{{--                <div class="error-message">--}}
+{{--                    <ul>--}}
+{{--                        @foreach ($errors->all() as $error)--}}
+{{--                            <li>{{ $error }}</li>--}}
+{{--                        @endforeach--}}
+{{--                    </ul>--}}
+{{--                </div>--}}
+{{--            @endif--}}
 
-            <div class="row justify-content-center py-4">
-                <div class="col-md-5 text-center">
-                    <input type="submit" class="profile-edit-btn" name="btnAddMore" value="Edit Profile"/>
-                </div>
-            </div>
+{{--            <div class="row justify-content-center py-4">--}}
+{{--                <div class="col-md-5 text-center">--}}
+{{--                    <input type="submit" class="profile-edit-btn" name="btnAddMore" value="Edit Profile"/>--}}
+{{--                </div>--}}
+{{--            </div>--}}
 
         </form>
-=======
+
         </div>
->>>>>>> c7f1aac7d67c4063e2892cff70d54ec2e5760920
     </div>
 
 @endsection
